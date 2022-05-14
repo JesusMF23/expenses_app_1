2de76cd0c79c48acc2e1fa0bc58f140d86ff9e9c
JesusMF23 <jesusmaldonadofernandez@gmail.com>
2022-05-14 11:20:49 +0200

    session-79&80-styling text

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -63,17 +63,37 @@ class MyHomePage extends StatelessWidget {
                             vertical: 10,
                             horizontal: 15,
                           ),
-                          child: Text(tx.amount.toString()),
+                          child: Text(
+                            tx.amount.toString(),
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple[800],
+                            ),
+                          ),
                           decoration: BoxDecoration(
                               border: Border.all(
-                            color: Color.fromARGB(255, 14, 123, 140),
+                            color: Color.fromARGB(255, 106, 27, 154),
                             width: 2,
                           )),
                           padding: EdgeInsets.all(10)),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Text(tx.title),
-                          Text(tx.date.toString())
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            tx.date.toString(),
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey[600],
+                            ),
+                          )
                         ],
                       )
                     ],
