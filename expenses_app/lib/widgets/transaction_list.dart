eeb207cfa9227231eecd6882ea707ffbf0673d97
JesusMF23 <jesusmaldonadofernandez@gmail.com>
2022-06-19 11:56:08 +0200

    108. Adding a ListTile Widget

--- expenses_app/lib/widgets/transaction_list.dart
+++ expenses_app/lib/widgets/transaction_list.dart
@@ -39,46 +39,67 @@ class TransactionList extends StatelessWidget {
               : ListView.builder(
                   itemBuilder: (context, index) {
                     return Card(
-                      child: Row(
-                        children: <Widget>[
-                          Container(
-                              margin: EdgeInsets.symmetric(
-                                vertical: 10,
-                                horizontal: 15,
-                              ),
-                              child: Text(
-                                "\$ ${transactions[index].amount.toStringAsFixed(2)}",
-                                // \$ is a special character in dart
-                                //interpolating string to call variables
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Theme.of(context).primaryColorDark,
-                                ),
-                              ),
-                              decoration: BoxDecoration(
-                                  border: Border.all(
-                                color: Theme.of(context).primaryColorLight,
-                                width: 2,
-                              )),
-                              padding: EdgeInsets.all(10)),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                transactions[index].title,
-                                style: Theme.of(context).textTheme.headline6,
-                              ),
-                              Text(
-                                DateFormat.yMEd("es_ES")
-                                    .format(transactions[index].date),
-                                style: Theme.of(context).textTheme.bodyText2,
-                              )
-                            ],
-                          )
-                        ],
+                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                      elevation: 5,
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 30,
+                          child: Padding(
+                            padding: EdgeInsets.all(6),
+                            child: FittedBox(
+                                child: Text("\$${transactions[index].amount}")),
+                          ),
+                        ),
+                        title: Text(
+                          transactions[index].title,
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        subtitle: Text(
+                          DateFormat.yMMMd().format(transactions[index].date),
+                        ),
                       ),
                     );
+                    // return Card(
+                    //   child: Row(
+                    //     children: <Widget>[
+                    //       Container(
+                    //           margin: EdgeInsets.symmetric(
+                    //             vertical: 10,
+                    //             horizontal: 15,
+                    //           ),
+                    //           child: Text(
+                    //             "\$ ${transactions[index].amount.toStringAsFixed(2)}",
+                    //             // \$ is a special character in dart
+                    //             //interpolating string to call variables
+                    //             style: TextStyle(
+                    //               fontSize: 20,
+                    //               fontWeight: FontWeight.bold,
+                    //               color: Theme.of(context).primaryColorDark,
+                    //             ),
+                    //           ),
+                    //           decoration: BoxDecoration(
+                    //               border: Border.all(
+                    //             color: Theme.of(context).primaryColorLight,
+                    //             width: 2,
+                    //           )),
+                    //           padding: EdgeInsets.all(10)),
+                    //       Column(
+                    //         crossAxisAlignment: CrossAxisAlignment.start,
+                    //         children: <Widget>[
+                    //           Text(
+                    //             transactions[index].title,
+                    //             style: Theme.of(context).textTheme.headline6,
+                    //           ),
+                    //           Text(
+                    //             DateFormat.yMEd("es_ES")
+                    //                 .format(transactions[index].date),
+                    //             style: Theme.of(context).textTheme.bodyText2,
+                    //           )
+                    //         ],
+                    //       )
+                    //     ],
+                    //   ),
+                    // );
                   },
                   itemCount: transactions.length,
                 ),
