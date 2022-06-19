02fffb0b1d7073f2a061823eebd534fde3784cf8
JesusMF23 <jesusmaldonadofernandez@gmail.com>
2022-06-19 10:43:40 +0200

    104. Creating Bars for our Chart

--- expenses_app/lib/widgets/chart.dart
+++ expenses_app/lib/widgets/chart.dart
@@ -21,7 +21,7 @@ class Chart extends StatelessWidget {
         }
       }
       return {
-        'day': DateFormat.E().format(weekDay),
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
       };
     });
@@ -34,10 +34,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(10),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Text('Chart'),
-        ],
+        children: groupedTransactionValues.map((data) {
+          return Text("${data['day']}: ${data['amount']}");
+        }).toList(),
       ),
     );
   }
