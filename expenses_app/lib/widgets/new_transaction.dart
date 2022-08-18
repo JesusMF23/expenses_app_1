32cf8e91d7cec7b1036b9db2b8e46be537f47dcf
JesusMF23 <jesusmaldonadofernandez@gmail.com>
2022-08-18 17:11:07 +0200

    128. Respecting the Softkeyboard Insets

--- expenses_app/lib/widgets/new_transaction.dart
+++ expenses_app/lib/widgets/new_transaction.dart
@@ -58,90 +58,96 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(
-                    labelText: "Title",
-                    labelStyle:
-                        TextStyle(color: Theme.of(context).primaryColorDark),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Theme.of(context).primaryColorLight)),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Theme.of(context).primaryColorLight))),
-                cursorColor: ThemeData.light().textSelectionTheme.cursorColor,
-                controller: _titleController,
-                onSubmitted: (_) {
-                  _submitData();
-                },
-                // onChanged: (val) {
-                //   titleInput = val;
-                // },
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: "Amount",
-                    labelStyle:
-                        TextStyle(color: Theme.of(context).primaryColorDark),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Theme.of(context).primaryColorLight)),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Theme.of(context).primaryColorLight))),
-                cursorColor: ThemeData.light().textSelectionTheme.cursorColor,
-                controller: _amountController,
-                keyboardType: TextInputType.number,
-                onSubmitted: (_) {
-                  _submitData();
-                },
-                // we use _ when flutter needs a parameter but we don't need it
-                // onChanged: (val) => amountInput = val,
-              ),
-              Container(
-                height: 70,
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(_selectedDate == null
-                          ? "No Date Chosen!"
-                          : "Picked Date: ${DateFormat.yMMMd().format(_selectedDate!)}"),
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+              top: 10,
+              left: 10,
+              right: 10),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "Title",
+                      labelStyle:
+                          TextStyle(color: Theme.of(context).primaryColorDark),
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).primaryColorLight)),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).primaryColorLight))),
+                  cursorColor: ThemeData.light().textSelectionTheme.cursorColor,
+                  controller: _titleController,
+                  onSubmitted: (_) {
+                    _submitData();
+                  },
+                  // onChanged: (val) {
+                  //   titleInput = val;
+                  // },
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "Amount",
+                      labelStyle:
+                          TextStyle(color: Theme.of(context).primaryColorDark),
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).primaryColorLight)),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).primaryColorLight))),
+                  cursorColor: ThemeData.light().textSelectionTheme.cursorColor,
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  onSubmitted: (_) {
+                    _submitData();
+                  },
+                  // we use _ when flutter needs a parameter but we don't need it
+                  // onChanged: (val) => amountInput = val,
+                ),
+                Container(
+                  height: 70,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(_selectedDate == null
+                            ? "No Date Chosen!"
+                            : "Picked Date: ${DateFormat.yMMMd().format(_selectedDate!)}"),
+                      ),
+                      TextButton(
+                          onPressed: _presentDatePicker,
+                          child: Text(
+                            "Choose Date",
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontWeight: FontWeight.bold),
+                          )),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _submitData();
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      Theme.of(context).primaryColor,
                     ),
-                    TextButton(
-                        onPressed: _presentDatePicker,
-                        child: Text(
-                          "Choose Date",
-                          style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                              fontWeight: FontWeight.bold),
-                        )),
-                  ],
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  _submitData();
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                    Theme.of(context).primaryColor,
+                    foregroundColor: MaterialStateProperty.all(
+                      Colors.white,
+                    ),
                   ),
-                  foregroundColor: MaterialStateProperty.all(
-                    Colors.white,
-                  ),
-                ),
-                child: Text("Add transaction",
-                    style: TextStyle(
-                        color: Theme.of(context).textTheme.button!.color)),
-              )
-            ]),
+                  child: Text("Add transaction",
+                      style: TextStyle(
+                          color: Theme.of(context).textTheme.button!.color)),
+                )
+              ]),
+        ),
       ),
     );
   }
